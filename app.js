4a024bcfa7b1360ac9e1b5b7c9b46272f85be99a
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-06-07 00:05:18 +0530

    feat: integrate dotenv for environment variable management and update project roadmap

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import deployRoutes from "./src/routes/deploy.routes.js";
 import logRoutes from "./src/routes/log.routes.js";
@@ -8,6 +9,6 @@ const port = process.env.PORT || 8080;
 
 app.use("/deploy", deployRoutes);
 
-app.use('/logs',logRoutes)
+app.use("/logs", logRoutes);
 
 app.listen(port, () => console.log(`server is running at port ${port}`));
